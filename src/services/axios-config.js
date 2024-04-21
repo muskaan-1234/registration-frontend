af34bba3af2bde9cc7e835ffe55eed57ff648f8c
SunSoftTechnologies <128724085+muskaan-1234@users.noreply.github.com>
2024-04-21 20:14:22 +0530

    updated axios-config

--- src/services/axios-config.js
+++ src/services/axios-config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-let baseURL = "http://localhost:2024";
+// let baseURL = "http://localhost:2024";
+let baseURL = "https://registration-backend-lxn5.onrender.com";
 
 const publicReq=axios.create({baseURL,});
 
